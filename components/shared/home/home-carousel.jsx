bda2acf5da9cbcc3f9ec2b12efef7326573da30c
Imran Hosein Khan Joy <greatkhanjoy@gmail.com>
2025-03-11 13:10:49 +0600

    Carousel Added

---
+++ components/shared/home/home-carousel.jsx
@@ -0,0 +1,60 @@
+'use client'
+
+import * as React from 'react'
+import Image from 'next/image'
+import Autoplay from 'embla-carousel-autoplay'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+export default function HomeCarousel({ items }) {
+  const plugin = React.useRef(
+    Autoplay({
+      delay: 3000,
+      stopOnMouseEnter: true,
+    })
+  )
+  return (
+    <Carousel
+      dir='ltr'
+      plugins={[plugin.current]}
+      className='w-full mx-auto'
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {items.map((item) => (
+          <CarouselItem key={item.title}>
+            <Link href={item.url}>
+              <div className='flex aspect-[16/6] items-center justify-center p-6 relative -m-1'>
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  fill
+                  className='object-cover'
+                  priority
+                />
+                <div className='absolute w-1/3 left-16 md:left-32 top-1/2  transform -translate-y-1/2'>
+                  <h2 className='text-xl md:text-6xl font-bold text-primary mb-4'>
+                    {item.title}
+                  </h2>
+                  <Button className='hidden md:block'>
+                    {item.buttonCaption}
+                  </Button>
+                </div>
+              </div>
+            </Link>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className='left-0 md:left-12' />
+      <CarouselNext className='right-0 md:right-12' />
+    </Carousel>
+  )
+}
